49368ec0fbb7a8bf41a90b4611214194ea5cfb02
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2022-11-07 08:43:24 +0100

    feat: include tailwind directives to main css file

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  css: [
+    '~/assets/style/index.css',
+  ],
   modules: [
     '@nuxtjs/tailwindcss'
   ]
