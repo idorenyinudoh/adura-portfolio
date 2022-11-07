ef09ba4dbab665b164e48ba3753a00d23c48124e
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2022-11-07 09:12:35 +0100

    feat: create background image and loading indicator on loading screen

---
+++ components/LoadingScreen.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="h-screen w-full relative">
+    <div class="fixed top-0 left-0 w-full h-full -z-10">
+      <img
+        class="w-full h-full object-cover"
+        src="~/assets/images/landscape-background.png"
+        alt="landscape background"
+      />
+    </div>
+    <div class="w-full h-14 md:h-20 xl:h-24 2xl:h-32 bg-white/30" />
+  </div>
+</template>
