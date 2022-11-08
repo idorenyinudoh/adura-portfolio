2cf04b18c3f99eccbfb5e364f024181bd02dbbc0
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2022-11-08 04:41:35 +0100

    feat: include font files and setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'clash-display': ['Clash Display', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
