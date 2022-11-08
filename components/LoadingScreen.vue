a87333a28cebecc4b901a6d0a74311706fa45ec1
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2022-11-08 05:01:14 +0100

    feat: include texts on loading screen

--- components/LoadingScreen.vue
+++ components/LoadingScreen.vue
@@ -8,5 +8,7 @@
       />
     </div>
     <div class="w-full h-14 md:h-20 xl:h-24 2xl:h-32 bg-white/30" />
+    <p class="font-bold text-white mt-9 ml-[8.3vw] text-8xl md:text-9xl">10</p>
+    <p class="font-extralight text-white/50 mt-3 md:mt-5 ml-[calc(8.3vw+12px)] text-4xl md:text-5xl">Do hang tight...</p>
   </div>
 </template>
