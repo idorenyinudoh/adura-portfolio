aefcd9f4bcaee36a43e4b36b76f97c4eb54873e7
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2022-11-21 07:15:54 +0100

    feat: implement animation of loading indicator and loading text

--- components/LoadingScreen.vue
+++ components/LoadingScreen.vue
@@ -1,5 +1,50 @@
+<script setup>
+import { gsap } from "gsap"
+import SplitType from 'split-type'
+
+let counter = ref(0)
+
+const onTextEnter = (el, done) => {
+  const split = new SplitType(el, {
+    types: 'words, chars'
+  })
+  const chars = split.chars
+  gsap.set(el, { autoAlpha: 1 })
+  gsap.fromTo(chars, { y: 100, opacity: 0 }, {
+    y: 0,
+    opacity: 1,
+    stagger: 0.05,
+    duration: 1.5,
+    ease: 'power4.out',
+    onComplete: done
+  })
+}
+
+const onTextLeave = (el, done) => {
+  gsap.fromTo(el.querySelectorAll('.char'), { y: 0 }, {
+    y: -100,
+    opacity: 0,
+    stagger: 0.05,
+    duration: 1.5,
+    ease: 'power4.out',
+    onComplete: done
+  })
+}
+
+onMounted(() => {
+  for(let i = 0; i <= 100; i++) {
+    setTimeout(() => {
+      counter.value = i
+    }, i * 50)
+  }
+  SplitType.create('.counter-text', {
+    types: 'words, chars',
+  })
+})
+</script>
+
 <template>
-  <div class="h-screen w-full relative">
+  <div class="loading-screen h-screen w-full relative">
     <div class="fixed top-0 left-0 w-full h-full -z-10">
       <img
         class="w-full h-full object-cover"
@@ -8,9 +53,29 @@
       />
     </div>
     <div class="relative w-full h-14 md:h-20 xl:h-24 2xl:h-32 bg-white/30">
-      <div class="absolute top-0 left-0 h-full w-0 bg-adura-purple" />
+      <div class="absolute top-0 left-0 h-full bg-adura-purple transition-all ease-linear duration-200" :style="{width: `${counter}%`}" />
     </div>
-    <p class="font-bold text-white mt-9 ml-[8.3vw] text-8xl md:text-9xl">10</p>
+    <div class="relative font-bold text-white mt-9 ml-[8.3vw] text-8xl md:text-9xl h-24 md:h-32">
+      <Transition @enter="onTextEnter" @leave="onTextLeave">
+        <p v-if="counter <= 10" class="counter-text absolute">0</p>
+        <p v-else-if="counter > 10 && counter <= 20" class="counter-text absolute">10</p>
+        <p v-else-if="counter > 20 && counter <= 30" class="counter-text absolute">20</p>
+        <p v-else-if="counter > 30 && counter <= 40" class="counter-text absolute">30</p>
+        <p v-else-if="counter > 40 && counter <= 50" class="counter-text absolute">40</p>
+        <p v-else-if="counter > 50 && counter <= 60" class="counter-text absolute">50</p>
+        <p v-else-if="counter > 60 && counter <= 70" class="counter-text absolute">60</p>
+        <p v-else-if="counter > 70 && counter <= 80" class="counter-text absolute">70</p>
+        <p v-else-if="counter > 80 && counter <= 90" class="counter-text absolute">80</p>
+        <p v-else-if="counter > 90 && counter < 100" class="counter-text absolute">90</p>
+        <p v-else-if="counter === 100" class="counter-text absolute">100</p>
+      </Transition>
+    </div>
     <p class="font-extralight text-white/50 mt-3 md:mt-5 ml-[calc(8.3vw+12px)] text-4xl md:text-5xl">Do hang tight...</p>
   </div>
 </template>
+
+<style scoped>
+.counter-text {
+  clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);
+}
+</style>
