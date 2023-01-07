21892664459e2f5330394d0d634f14d9c191e7df
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-01-07 13:23:38 +0100

    feat: create and style heading text on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <p>bruh</p>
+  <div class="h-screen w-full relative">
+    <div class="fixed top-0 left-0 w-full h-full -z-10">
+      <img
+        class="w-full h-full object-cover"
+        src="~/assets/images/landscape-background.png"
+        alt="landscape background"
+      />
+    </div>
+    <div class="absolute">
+      <div class="font-bold text-white ml-[8.3vw] mt-24 md:mt-28 xl:mt-32 2xl:mt-40 text-4xl md:text-7xl lg:text-8xl">
+        <h1>ADURAGBEMI</h1>
+        <div class="flex">
+          <h1 class="w-max">ABIOLA</h1>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
