5a45575812037431c33ada0fbb710d33779dc6cd
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-07-21 20:51:37 +0100

    feat: create heading and intro paragraph of work page

---
+++ pages/work.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="pr-[12.9vw] xl:pr-44">
+    <h1 class="font-bold text-adura-black text-4xl sm:text-5xl md:text-7xl lg:text-8xl">
+      SOME OF MY RECENT WORKS
+    </h1>
+    <p class="mt-12 text-adura-black text-base md:text-lg lg:text-2xl font-light">
+      Over the past few months, I have busied myself with sharpening my skills and polishing my craft so that when the time came for me to create a portfolio, I would be proud of my work, be confident in its value and believe in its ability to make a remarkable contribution.
+      <br>
+      <br>
+      Here are some projects I've worked on. I hope they inspire you and most importantly, I hope you are impressed by them. Make yourself comfortable and enjoy the show.
+    </p>
+  </div>
+</template>
