e9167f26bf430f1a4dfa89a7e54d9d82fb44bd3f
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-08-29 05:26:34 +0100

    feat: update sections on about page(except contact section) with new design

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,26 @@
 <script setup lang="ts">
-const certificationsTable = ref({
+const certificationsTable = {
   headers: [
     'course',
     'institution',
     'year'
   ],
   rows: [
+    {
+      course: 'Webflow101',
+      institution: 'Webflow University',
+      year: '2023'
+    },
+    {
+      course: 'Gamification: How To Create Engaging User Experience',
+      institution: 'Interaction Design Foundation (IxDF)',
+      year: '2023'
+    },
+    {
+      course: 'How to Create Intuitive Products by Imitating Physicality',
+      institution: 'Interaction Design Foundation (IxDF)',
+      year: '2022'
+    },
     {
       course: 'Mobile User Experience (UX) Design',
       institution: 'Interaction Design Foundation (IxDF)',
@@ -57,23 +72,26 @@ const certificationsTable = ref({
       year: '2016 - 2021'
     }
   ]
-})
-const skills = ref([
+}
+const skills = [
+  'No Code Development',
+  'Critical Thinking',
+  'User Research',
+  'Design Systems',
   'Usability Testing',
   'Strategy and Design',
   'Mobile App and Web Design',
   'Problem Solving',
-  'Critical Thinking',
-  'User Research',
-  'Design Systems',
-  'Wireframing and Mockup Designs',
+  'Wireframing',
   'Design Thinking',
   'User Experience Design',
+  'High Fidelity Mockups',
   'User Interface Design',
   'Interactive Prototyping',
-  'Accessibility'
-])
-const awardsTable = ref({
+  'Accessibility',
+  'A/B Testing'
+]
+const awardsTable = {
   headers: [
     'platform',
     'award-or-article',
@@ -96,19 +114,17 @@ const awardsTable = ref({
       year: '2018'
     }
   ]
-})
+}
 </script>
 
 <template>
-  <h1 class="font-bold text-adura-black text-4xl sm:text-5xl md:text-7xl lg:text-8xl">
-    ABOUT ME
-  </h1>
-  <p class="my-12 text-adura-black text-base md:text-lg lg:text-2xl font-light">
+  <TextBaseH1 text="MEET ADURA" />
+  <TextBaseP>
     Hi, I am Adura, a user experience designer. I have an experience in mobile app and web design, wire-framing and mockup design, user research, interactive prototyping illustration amongst others.
     <br>
     <br>
     Identifying and understanding the human needs is crucial for creating useful and usable products. I relish the moments when my skill and knowledge empowers people, thereby making life easier.
-  </p>
+  </TextBaseP>
   <div class="grid grid-cols-2 lg:grid-cols-[2fr_1fr_1fr] gap-4 lg:gap-6 xl:gap-10">
     <div class="col-start-1 col-end-3 lg:col-end-2 lg:row-start-1 lg:row-end-3">
       <img class="w-full h-full object-cover rounded-[32px] lg:grayscale lg:hover:grayscale-0 transition-all duration-[400ms] ease-linear" src="~/assets/images/about/adura-working.png" alt="adura working">
@@ -123,20 +139,27 @@ const awardsTable = ref({
       <img class="w-full h-full object-cover rounded-[32px] lg:grayscale lg:hover:grayscale-0 transition-all duration-[400ms] ease-linear" src="~/assets/images/about/adura-talking.png" alt="adura communicating with teammate">
     </div>
   </div>
-  <p class="my-12 text-adura-black text-base md:text-lg lg:text-2xl font-light">
-    In my spare time I enjoy podcasting, documenting my journey via writing and listening to good music. I occasionally do enjoy a good movie time too. I am a goal getter and a dream chaser, I believe in the “leaving lasting impression, in people and through my designs. 
-    <br>
-    <br>
-    If you believe I am good fit for your team or project, please reach out and let's have a chat. I can't wait to hear from you!
-  </p>
   <TextBaseH2 text="CERTIFICATIONS" />
   <BaseTable :table-data="certificationsTable" />
   <TextBaseH2 text="SKILLS" />
   <div class="flex flex-wrap gap-2 md:gap-3 lg:gap-6">
-    <p v-for="(skill, index) in skills" :key="index" class="px-3 py-1 md:px-6 md:py-3 lg:px-12 lg:py-6 border border-solid border-adura-black rounded-[48px] text-base md:text-lg lg:text-2xl font-normal">
+    <p v-for="(skill, index) in skills" :key="index" class="px-6 py-3 lg:px-12 lg:py-6 border border-solid border-adura-black rounded-[48px] text-base md:text-lg lg:text-2xl font-normal">
       {{ skill }}
     </p>
   </div>
+  <TextBaseH2 text="INTERESTS" />
+  <TextBaseP>
+    One thing you know about me is that I always have some form of headphones on my person.
+    <br>
+    <br>
+    This is because I absolutely enjoy listening to music and across MULTIPLE genres. From Afrobeats (Burna Boy, Wizkid, Adekunle Gold, etc) to Highlife (Cavemen) and Live praise concerts (Bidemi Olaoba, Femi Praise, Mega 99) to Old School Apala (Haruna Ishola and the likes) to Heartbreak songs (Passenger, Jon Bellion, Lewis Capaldi, etc) to UK Rap (Stormzy, Dave) to “New Cats” HipHop (Fireboy, Rema, Bnxn, Ladipoe, etc) to Nigerian Rap Artistes (MI, Vector).
+    <br>
+    <br>
+    I also enjoy long form audio content like audiobooks (Jordan Person, Simon Sinek, Mark Manson are my OGs), YouTube interviews and podcasts (spoiler I have a podcast with about 30k listens so far). Comedy standups are also a personal favorite (Dave Chapelle, Trevor Noah, Kevin Hart, Matt Rife).
+    <br>
+    <br>
+    I do enjoy seeing movies and I especially love movies of the investigative type, Sherlock and the likes. I also take random courses of YouTube sometimes not even design related.
+  </TextBaseP>
   <TextBaseH2 text="AWARDS AND PUBLICATIONS" />
   <BaseTable :table-data="awardsTable" />
   <TextBaseH2 text="REACH OUT" />
