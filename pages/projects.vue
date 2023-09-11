e0f625f2d6c93e0f2e4638335a030b673ba0af4f
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-09-11 13:23:10 +0100

    feat: delete work page and replace with projects page containing hero

---
+++ pages/projects.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="hero -mx-[8.3vw] xl:-mx-[120px] -mt-16 md:-mt-24 h-[80vh] md:h-[calc(100vh-173px)] xl:h-[calc(100vh-225px)] flex justify-center items-center">
+    <TextBaseH1 text="RECENT WORKS OF ART" class="text-white text-center mx-[15%]" />
+  </div>
+</template>
+
+<style scoped>
+.hero {
+  background: linear-gradient(rgb(26 26 26 / 87%) 0%, rgb(26 26 26 / 87%) 100%), url(@/assets/images/projects/betasms.png);
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+</style>
