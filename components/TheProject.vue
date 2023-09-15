5a96dc6eca625be05f8e4b8999d0e3c47573170d
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-09-15 10:32:17 +0100

    feat: create component and page for individual project page

---
+++ components/TheProject.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <BaseH1 text="kólé" />
+    <div class="flex flex-col lg:flex-row lg:items-center lg:justify-between lg:gap-x-[10.1%]">
+      <BaseP>
+        Kólé is dedicated to revolutionising construction planning and management in Africa. Empowering aspiring home owners and construction professionals alike with cutting-edge digital solutions that simplify and optimise every aspect of the construction journey.
+      </BaseP>
+      <a class="w-max px-6 md:px-8 lg:px-10 py-2 md:py-2.5 lg:py-3 rounded-[56px] border border-solid border-adura-black bg-adura-black/10 hover:bg-adura-black/70 whitespace-nowrap text-adura-black hover:text-white font-normal text-sm md:text-base lg:text-xl transition-all duration-200 ease-linear" href="">View Live Site</a>
+    </div>
+    <div class="pt-[75%] sm:pt-[60%] lg:pt-[40.5%] -mx-[8.3vw] lg:mx-0 bg-[#D9D9D9] mt-16 lg:mt-4 rounded-none lg:rounded-2xl" />
+    <BaseH2 text="PROJECT OBJECTIVES" class="!mb-6 md:!mb-8 lg:!mb-10" />
+    <BaseUl>
+      <li>Create a design system for the company.</li>
+      <li>Create a highly responsive web app that is compatible for multiple devices.</li>
+      <li>Create a mobile app solution.</li>
+      <li>Creating two landing pages, one to accumulate emails to build the waitlist and the other to launch the software.</li>
+      <li>Carry out usability testing from time to time to make sure that the design is adequately optimised for the problem the team is poised to solve.</li>
+      <li>Track and monitor usage of the software while creating a better user experience from a data driven standpoint.</li>
+    </BaseUl>
+    <div class="flex flex-col md:flex-row items-center gap-y-8 md:gap-x-8 md:gap-y-0">
+      <div class="bg-[#D9D9D9] rounded-lg md:rounded-2xl pt-[75%] sm:pt-[60%] md:pt-[43.5%] w-full"></div>
+      <div class="bg-[#D9D9D9] rounded-lg md:rounded-2xl pt-[75%] sm:pt-[60%] md:pt-[43.5%] w-full"></div>
+    </div>
+    <BaseH2 text="DESIGN PROCESS" class="!mb-6 md:!mb-8 lg:!mb-10" />
+    <BaseUl>
+      <li><span class="font-medium">Problem statement, understanding and research:</span> This was the very first step in my design process as I had to carefully explore the existing user interface, the current website to understand their services and ask questions within the team to make sure the design solves real world problems.</li>
+      <li><span class="font-medium">Design system:</span> Create a global design system that caters to both the mobile application and back-office and customise it to fit SCIART's brand identity.</li>
+      <li><span class="font-medium">Wireframing and prototyping:</span> Creating the design system, I then proceeded to creating sketches of how the new feature(s) will look before finally moving on to design software. PS: I especially enjoy this process using pen and paper.</li>
+      <li><span class="font-medium">High fidelity design iterations:</span> This process involves ideating the low fidelity designs (wireframes) into useful and usable interface.</li>
+      <li><span class="font-medium">Review and further iterations:</span> The design is then reviewed by the CEO and when green-lit, pushed for development.</li>
+    </BaseUl>
+    <BaseH2 text="CHALLENGES" class="!mb-6 md:!mb-8 lg:!mb-10" />
+    <BaseUl>
+      <li>Designing effective ways to visualise financial data and analytics within the admin back-office can be complex.</li>
+      <li>Due to the fact that where was an existing iteration of the product, I had to go back and forth to make sure this new iteration solves the issues the existing one had.</li>
+      <li>Working within such short timeframe was highly challenging and I was able to push myself to meet the deadline though we had a few close calls.</li>
+    </BaseUl>
+    <BaseH2 text="LESSONS LEARNED" class="!mb-6 md:!mb-8 lg:!mb-10" />
+    <BaseUl>
+      <li>I actively sought and incorporated feedback from a diverse set of stakeholders, including users, developers, and product managers, to refine my designs.</li>
+      <li>I learned to maintain well-organised design documentation, style guides, and UI component libraries to streamline collaboration and future updates.</li>
+      <li>I especially learned what it meant to work with friends and reminded me the true power of collaborations.</li>
+    </BaseUl>
+    <TheContact />
+  </div>
+</template>
