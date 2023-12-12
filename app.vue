3f40fccdb6d144a43fbafb47fac9bbb0a914e509
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-12-12 01:01:20 +0100

    refactor: simplify implementation of different padding on index page

--- app.vue
+++ app.vue
@@ -1,8 +1,6 @@
 <script setup lang="ts">
-const route = useRoute()
 const store = usePreloadImagesStore()
 const { imagesHaveLoaded } = storeToRefs(store)
-const isIndexPage = computed(() => route.path === '/')
 
 onMounted(() => {
   if (!imagesHaveLoaded.value) {
@@ -14,14 +12,7 @@ onMounted(() => {
 <template>
   <div>
     <Preloader v-if="!imagesHaveLoaded" />
-    <div v-else
-      :class="[
-        'px-[8.3vw] xl:px-[120px] pb-14 lg:pb-32',
-        isIndexPage ? 'pt-16' : 'pt-[calc(64px+2.5rem)]',
-        isIndexPage ? 'md:pt-24' : 'md:pt-[calc(73px+4rem)]',
-        isIndexPage ? 'lg:pt-32' : 'lg:pt-[calc(93px+5rem)]'
-      ]"
-    >
+    <div v-else class="bodyyy px-[8.3vw] xl:px-[120px] pt-[calc(64px+2.5rem)] md:pt-[calc(73px+4rem)] lg:pt-[calc(93px+5rem)] pb-14 lg:pb-32">
       <NuxtPage />
       <TheMenu />
     </div>
