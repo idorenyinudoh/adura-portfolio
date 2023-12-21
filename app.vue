cd961985ed86806410f9488cc31b7a53bb52978d
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-12-21 15:00:56 +0100

    feat: include all favicons in app.vue

--- app.vue
+++ app.vue
@@ -27,9 +27,22 @@ useHead({
   ],
   link: [
     {
-      rel: 'icon',
-      href: '/favicon.ico'
+      rel: 'apple-touch-icon',
+      sizes: '180x180',
+      href: '/apple-touch-icon.png'
     },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '32x32',
+      href: '/favicon-32x32.png'
+    },
+    {
+      rel: 'icon',
+      type: 'image/png',
+      sizes: '16x16',
+      href: '/favicon-16x16.png'
+    }
   ],
   htmlAttrs: {
     lang: 'en'
