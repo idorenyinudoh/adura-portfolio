4d9c27e48662296109b30699cfe87a1604219d1d
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2023-12-24 23:45:30 +0100

    fix: implement preloader and homepage intro animations such that page content exists on initial visit

--- app.vue
+++ app.vue
@@ -86,7 +86,7 @@ onMounted(() => {
 <template>
   <div>
     <Preloader v-if="!imagesHaveLoaded" />
-    <div v-else class="bodyyy px-[8.3vw] xl:px-[120px] pt-[calc(64px+2.5rem)] md:pt-[calc(73px+4rem)] lg:pt-[calc(93px+5rem)] pb-14 lg:pb-32">
+    <div :class="['bodyyy px-[8.3vw] xl:px-[120px] pt-[calc(64px+2.5rem)] md:pt-[calc(73px+4rem)] lg:pt-[calc(93px+5rem)] pb-14 lg:pb-32', { 'hidden': !imagesHaveLoaded }]">
       <NuxtPage :transition="transitionObject" />
       <TheMenu />
     </div>
