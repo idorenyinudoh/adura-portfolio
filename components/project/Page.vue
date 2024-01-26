e327a3419bbf9f987b07bd2bb60a1301b2b5b632
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2024-01-26 15:08:25 +0100

    refactor: remove hover state from 'view site' button in project page

--- components/project/Page.vue
+++ components/project/Page.vue
@@ -55,7 +55,7 @@ defineProps({
       <h1>{{ projectName }}</h1>
       <div class="flex flex-col lg:flex-row lg:items-center lg:justify-between lg:gap-x-[10.1%]">
         <p class="base-text">{{ projectDescription }}</p>
-        <NuxtLink v-if="deployLink" class="w-max px-6 md:px-8 lg:px-10 py-2 md:py-2.5 lg:py-3 rounded-[56px] border border-solid border-adura-black bg-adura-black/10 hover:bg-adura-black/70 whitespace-nowrap text-adura-black hover:text-white font-normal text-sm md:text-base lg:text-xl transition-all duration-200 ease-linear" :href="deployLink" target="_blank">View Live Site</NuxtLink>
+        <NuxtLink v-if="deployLink" class="w-max px-6 md:px-8 lg:px-10 py-2 md:py-2.5 lg:py-3 rounded-[56px] border border-solid border-adura-black bg-adura-black/10 whitespace-nowrap text-adura-black font-normal text-sm md:text-base lg:text-xl" :href="deployLink" target="_blank">View Live Site</NuxtLink>
         <p v-else-if="projectName.toLowerCase() !== 'hydrogen'" class="w-max px-6 md:px-8 lg:px-10 py-2 md:py-2.5 lg:py-3 rounded-[56px] border border-solid border-adura-black bg-adura-black/10 whitespace-nowrap text-adura-black font-normal text-sm md:text-base lg:text-xl">In Development</p>
       </div>
       <div class="relative pt-[75%] sm:pt-[60%] lg:pt-[40.5%] -mx-[8.3vw] lg:mx-0 mt-16 lg:mt-4">
