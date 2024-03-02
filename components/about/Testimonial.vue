4ab70a969b3ef5b9016fe4ad532eef19b46d7f2c
Idorenyin Udoh <idorenyinudoh10@outlook.com>
2024-03-02 09:41:24 +0100

    feat: hide awwwards banner on mobile

--- components/about/Testimonial.vue
+++ components/about/Testimonial.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts">
+// import { gsap } from 'gsap'
+// import SplitType from 'split-type'
+
 const props = defineProps<{
   headshot: string
   name: string
@@ -8,6 +11,94 @@ const props = defineProps<{
   devPitch?: string
   devImage?: string
 }>()
+
+// const onTextEnter = (el: HTMLElement, done: any) => {
+//   if (!el.querySelector('.char')) {
+//     SplitType.create(el, {
+//       types: 'words, chars'
+//     })
+//   }
+
+//   gsap.fromTo(el.querySelectorAll('.char'), { y: 15, opacity: 0 }, {
+//     y: 0,
+//     opacity: 1,
+//     stagger: 0.05,
+//     duration: 1.5,
+//     delay: 10,
+//     ease: 'power4.out',
+//     onComplete: done
+//   })
+// }
+
+// const onTextLeave = (el: HTMLElement, done: any) => {
+//   if (!el.querySelector('.char')) {
+//     SplitType.create(el, {
+//       types: 'words, chars'
+//     })
+//   }
+//   const tl = gsap.timeline()
+//   tl.fromTo(el.querySelectorAll('.word'), { y: 0 }, {
+//     y: -15,
+//     opacity: 0,
+//     stagger: 0.05,
+//     duration: 1.5,
+//     ease: 'power4.out',
+//     // onComplete: done
+//   })
+
+//   if (pitchIsVisible.value) {
+//     const parentElement = el?.parentElement
+//     let eventualBackgroundSize: number = 0
+
+//     switch (parentElement?.offsetWidth) {
+//       case 360:
+//         eventualBackgroundSize = 150
+//         break
+//       case 480:
+//         eventualBackgroundSize = 125
+//         break
+//       default:
+//         eventualBackgroundSize = 100
+//         break
+//     }
+
+//     tl.set(parentElement, {
+//       backgroundAttachment: 'fixed',
+//       backgroundPositionX: 'center',
+//       backgroundPositionY: 'center',
+//       backgroundRepeat: 'no-repeat',
+//       backgroundSize: '85%',
+//       // onComplete: done
+//     })
+//     .to(parentElement, {
+//       duration: 3,
+//       borderColor: '#1A1A1A',
+//       backgroundImage: 'linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.80) 39.92%), url("extra-image.jpg")',
+//       backgroundSize: eventualBackgroundSize,
+//       ease: 'power4.out',
+//       onComplete: done
+//     })
+//     // .to(, {
+//     //   duration: 3,
+//     //   backgroundSize: 'cover',
+//     //   ease: 'power4.out',
+//     //   onComplete: done
+//     // })
+//   }
+// }
+
+// const pitchIsVisible = ref(false)
+// const timeout = ref(0)
+
+// const showPitch = () => {
+//   timeout.value = setTimeout(() => {
+//     pitchIsVisible.value = true
+//   }, 3000)
+// }
+// const hidePitch = () => {
+//   clearTimeout(timeout.value)
+//   pitchIsVisible.value = false
+// }
 </script>
 
 <template>
@@ -26,5 +117,22 @@ const props = defineProps<{
       </div>
     </div>
     <p class="mt-5 lg:mt-7 xl:mt-8 text-adura-black text-sm md:text-base lg:text-xl font-normal">{{ props.testimonial }}</p>
+    <!-- <Transition @enter="onTextEnter" @leave="onTextLeave">
+      <p v-if="props.devPitch && pitchIsVisible" class="absolute w-[310px] sm:w-[430px] md:w-[638px] lg:w-[702px] xl:w-[846px] mt-5 lg:mt-7 xl:mt-8 text-white text-sm md:text-base lg:text-xl font-normal line-clamp-[11] sm:line-clamp-[8] md:line-clamp-6 lg:line-clamp-[7] xl:line-clamp-6" style="clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);">{{ props.devPitch }}</p>
+      <p v-else class="mt-5 lg:mt-7 xl:mt-8 text-adura-black text-sm md:text-base lg:text-xl font-normal line-clamp-[11] sm:line-clamp-[8] md:line-clamp-6 lg:line-clamp-[7] xl:line-clamp-6" style="clip-path: polygon(0 0, 100% 0, 100% 100%, 0% 100%);">{{ props.testimonial }}</p>
+    </Transition> -->
   </article>
 </template>
+
+<!-- <style scoped>
+.dev-pitch-container {
+  border-color: #1A1A1A;
+  background-image: linear-gradient(180deg, rgba(0, 0, 0, 0.00) 0%, rgba(0, 0, 0, 0.80) 39.92%), url('extra-image.jpg');
+  background-attachment: fixed;
+  background-position-x: right;
+  background-position-y: top;
+  background-repeat: no-repeat;
+  background-size: cover;
+  transition: background-image 250ms ease-in-out;
+}
+</style> -->
